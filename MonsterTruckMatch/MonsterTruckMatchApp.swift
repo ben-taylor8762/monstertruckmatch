5d012ed81c9bed9d72bbf8b5b0b7cd18c68d80b9
Ben Taylor <ben.taylor8762@gmail.com>
2025-07-02 21:58:54 -0600

    Initial Commit

---
+++ MonsterTruckMatch/MonsterTruckMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterTruckMatchApp.swift
+//  MonsterTruckMatch
+//
+//  Created by Ben Taylor on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct MonsterTruckMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MonsterTruckMatchTests/MonsterTruckMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterTruckMatchTests.swift
+//  MonsterTruckMatchTests
+//
+//  Created by Ben Taylor on 7/2/25.
+//
+
+import Testing
+@testable import MonsterTruckMatch
+
+struct MonsterTruckMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
